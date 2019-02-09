48b29dc84dafa2824053c79e9382b640967cb285
Patrick <46059506+OOXXXX@users.noreply.github.com>
2019-02-09 14:21:00 +0800

    Update GiantUITests.swift

--- GiantUITests.swift
+++ GiantUITests.swift
@@ -8,9 +8,11 @@
 
 import XCTest
 
-class GiantUITests: XCTestCase {
+class GiantUITests: XCTestCase 
+{
 
-    override func setUp() {
+    override func setUp() 
+    {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -22,11 +24,13 @@ class GiantUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
+    override func tearDown() 
+    {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() 
+    {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
