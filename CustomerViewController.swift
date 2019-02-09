0a51cf36c05bddc04c331c96c8fa72acc9076bba
Patrick <46059506+OOXXXX@users.noreply.github.com>
2019-02-09 14:15:01 +0800

    Add files via upload

---
+++ CustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerViewController.swift
+//  Giant
+//
+//  Created by Rhapsody on 2019/1/18.
+//  Copyright © 2019 Back to Bedlam. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Giant
+//
+//  Created by Rhapsody on 2019/1/18.
+//  Copyright © 2019 Back to Bedlam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        appendThreeViews()
+    }
+
+    func appendThreeViews()
+    {
+        let firstView = UIView(frame: CGRect(x: 40, y: 80, width: 100, height: 100))
+        firstView.backgroundColor = UIColor.green
+        self.view.addSubview(firstView)
+        
+        let secondView = UIView(frame: CGRect(x: 40, y: 80, width: 120, height: 120))
+        firstView.backgroundColor = UIColor.blue
+        self.view.addSubview(secondView)
+        
+        let thirdView = UIView(frame: CGRect(x: 40, y: 80, width: 140, height: 140))
+        thirdView.backgroundColor = UIColor.purple
+        self.view.addSubview(thirdView)
+        
+    }
+
+
+}
+
+
+
